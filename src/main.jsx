f2212614ed821d7e5d50c021e046f3d803eb484b
Cesar Rejon <crejonec@gmail.com>
2024-10-15 01:43:19 -0600

    Add react router with components

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,22 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Favorites from './componets/Favorites.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  },
+  {
+    path: "/favorites",
+    element: <Favorites/>
+  }
+])
 
 createRoot(document.getElementById('root')).render(
-<main className="className=absolute top-0 z-[-2] h-min-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]">
-  <App />
+<main className="className=absolute top-0 z-[-2] min-h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]">
+  <RouterProvider router={router} />
 </main>
 )
