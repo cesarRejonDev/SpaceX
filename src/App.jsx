f617f32365711535fedde9da8c1133a15991c54e
Cesar Rejon <crejonec@gmail.com>
2024-10-14 22:31:51 -0600

    add css

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
           setLaunches(data);   // Guarda datos del estado
           setLoading(false);   // Cambia el estado de carga
         })
-        // .catch(error => console.error('Error al obtener los lanzamientos:', error));
+      .catch(error => console.error('Error al obtener los lanzamientos:', error));
     }, []); // El array vacío asegura se ejecute una vez  cuando se monte el componente
 
 
@@ -25,22 +25,22 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
       }
 
     return (
-      <div className="container mx-auto p-5  h-full w-full  bg-black class=absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px] text-slate-200">
-      <h1 className="text-4xl text-center mb-4 bebas-neue-normal font-bold text-neutral-400">SpaceX Launches</h1>
-      <NavBar />
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {launches.map(launch => (
-          <RocketCard
-            key={launch.id}
-            name={launch.name}
-            date={launch.date_utc}
-            details={launch.details}
-            image={launch.links.patch.small}
-            success={launch.success}
-          />
-        ))}
-      </div>
-    </div>
+      <section className="max-w-screen-xl px-5 mx-auto">
+        <h1 className="text-4xl text-center mb-4 bebas-neue-normal font-bold text-neutral-400">SpaceX Launches</h1>
+        <NavBar />
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {launches.map(launch => (
+            <RocketCard
+              key={launch.id}
+              name={launch.name}
+              date={launch.date_utc}
+              details={launch.details}
+              image={launch.links.patch.small}
+              success={launch.success}
+            />
+          ))}
+        </div>
+      </section>
     )
  }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,5 +3,7 @@ import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
+<main className="className=absolute top-0 z-[-2] h-min-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]">
   <App />
+</main>
 )
