50acf0374f13a68ae71b8e8ba419afc7e7056a22
Cesar Rejon <crejonec@gmail.com>
2024-10-15 02:15:28 -0600

    delete heroicons

--- src/componets/NavBar.jsx
+++ src/componets/NavBar.jsx
@@ -1,40 +1,36 @@
 import React from "react";
-import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
+import { Link } from "react-router-dom";
+import Magnifying from "./icons/Magnifying";
 
 
 const Navbar = () => {
   return (
-    <nav className="p-1 sticky top-0 z-50 mb-4">
-      <div className="container mx-auto flex justify-between items-center">
+    <nav className="sticky top-0 z-50 mb-4">
+      <div className="mx-auto flex justify-between items-center">
     
         {/* Search Bar */}
         <div className="flex items-center space-x-2">
           <input
             type="search"
             placeholder="Search Launches..."
-            className="bg-neutral-500 p-2x1 rounded-md border border-neutral-200 focus:outline-none focus:ring-2 focus:ring-white focus:border-transparent"
+            className="bg-transparent p-2 rounded-md border border-neutral-200 focus:outline-none focus:ring-2 focus:ring-white focus:border-transparent"
           />
-          <button className="bg-neutral-400 text-slate-600 p-1 rounded-full hover:bg-gray-300">
-          <MagnifyingGlassIcon className="h-5 w-5 text-slate-500" />
+          <button className="bg-blue-950 p-1 rounded-full hover:bg-blue-900">
+            <Magnifying className="h-5 w-5 text-white" />
           </button>
         </div>
 
         {/* Nav Links */}
         <ul className="flex space-x-4 text-neutral-400">
           <li>
-            <a href="/" className="hover:underline hover:text-white">
+            <Link to="/" className="hover:underline hover:text-white">
               Home
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="/favorite" className="hover:underline hover:text-white">
-              Favorite
-            </a>
-          </li>
-          <li>
-            <a href="/contact" className="hover:underline hover:text-white">
-              Contact
-            </a>
+            <Link to="/favorites" className="hover:underline hover:text-white">
+              Favorites
+            </Link>
           </li>
         </ul>
       </div>
